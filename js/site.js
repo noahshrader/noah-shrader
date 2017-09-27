52e1245e73054254ef09ad7f042ea1fe4b7a7122
Noah Shrader <noahshrader@gmail.com>
2017-09-27 08:07:17 -0500

    Remove old/unneeded scripts

--- js/site.js
+++ js/site.js
@@ -9,56 +9,10 @@ function fadeTop() {
        welcomeBg.css({'opacity' : (1 - st/320)});
        welcomeMain.css({'opacity' : (1 - st/320)});
     }
-    if((welcomeMain).css('opacity') <= 0) { 
+    if((welcomeMain).css('opacity') <= 0) {
         welcomeMain.addClass('hide');
     } else {
         welcomeMain.removeClass('hide');
-    }
-}
-
-function noahScroll() {
-    scrollPos = $(this).scrollTop();
-    $('.noah').css({
-        'background-position' : '50% ' + (-scrollPos/40 + 20) + "px"
-    });
-}
-
-function scrollEvents() {
-    var distancetop = $('.projects').offset().top,
-        distancelast = $('.project-feature.last').offset().top,
-        pkradiodistance = $('#pkradio').offset().top - 550,
-        tagticiandistance = $('#tagtician').offset().top - 550,
-        pullrdistance = $('#pullr').offset().top - 550,
-        $window = $(window);
-    // Top of container
-    if ($window.scrollTop() >= distancetop) {
-        $('.project-nav').addClass('active');
-    } else {
-        $('.project-nav').removeClass('active');
-    }
-    // PK Radio
-    if ($window.scrollTop() >= pkradiodistance) {
-        $('.project-nav').addClass('pkradio');
-    } else {
-        $('.project-nav').removeClass('pkradio');
-    }
-    // Tagtician
-    if ($window.scrollTop() >= tagticiandistance) {
-        $('.project-nav').addClass('tagtician');
-    } else {
-        $('.project-nav').removeClass('tagtician');
-    }
-    // Pullr
-    if ($window.scrollTop() >= pullrdistance) {
-        $('.project-nav').addClass('pullr');
-    } else {
-        $('.project-nav').removeClass('pullr');
-    }
-    // Bottom of container
-    if ($window.scrollTop() >= distancelast) {
-        $('.project-nav').addClass('hide');
-    } else {
-        $('.project-nav').removeClass('hide');
     }
 }
 
@@ -67,7 +21,7 @@ $(function() {
     // Top navigation scroll
     $('.welcome .about-link a').click(function() {
         $('html, body').animate({
-            scrollTop: $('.info').offset().top
+            scrollTop: $('.info').offset().top - 250
         }, 600);
     });
     $('.welcome .work-link a').click(function() {
@@ -86,12 +40,7 @@ $(function() {
         }, 600);
     });
 
-    // Project navigation
-    $('#nav').onePageNav();
-
     fadeTop();
-    noahScroll();
-    scrollEvents();
 });
 
 $(window).resize(function() {
@@ -100,6 +49,4 @@ $(window).resize(function() {
 
 $(window).on('scroll', function() {
     fadeTop();
-    noahScroll();
-    scrollEvents();
 });
