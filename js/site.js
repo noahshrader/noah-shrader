b14bd56a3407495a5c8269012b70bd9cda251c0f
Noah Shrader <noahshrader@gmail.com>
2018-08-01 23:31:41 -0500

    Implement new structure and styling for redesign

--- js/site.js
+++ js/site.js
@@ -1,3 +1,15 @@
 $(function() {
-
+  $('.view-library').click(function() {
+    $('.book-list, .book-list-close').addClass('open')
+    $('body').addClass('stuck')
+  })
+  $('.book-list-close').click(function() {
+    $(this).removeClass('open')
+    $('.book-list').removeClass('open')
+    $('body').removeClass('stuck')
+  })
 });
+
+$(window).load(function() {
+  $('body').addClass('active')
+})
