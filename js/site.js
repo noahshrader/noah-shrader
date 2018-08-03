5e5f0b14e1ffd20568bd1662b23f0835d647e354
Noah Shrader <noahshrader@gmail.com>
2018-08-02 23:10:25 -0500

    Improve library modal interaction

--- js/site.js
+++ js/site.js
@@ -1,11 +1,12 @@
 $(function() {
   $('.view-library').click(function() {
-    $('.book-list, .book-list-close').addClass('open')
+    $('.book-list, .book-list-close, main').addClass('open')
     $('body').addClass('stuck')
   })
+
   $('.book-list-close').click(function() {
     $(this).removeClass('open')
-    $('.book-list').removeClass('open')
+    $('.book-list, main').removeClass('open')
     $('body').removeClass('stuck')
   })
 });
