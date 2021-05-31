8da34b75d3edf23e7a2db9850f1e5eee7164a7e3
Noah Shrader <noahshrader@gmail.com>
2021-05-31 13:13:35 -0400

    Update styles and scripts

--- js/site.js
+++ js/site.js
@@ -1,35 +1,37 @@
-$(function() {
-  $('.view-library').click(function() {
+$(function () {
+  $('.view-library').click(function () {
     $('.read').addClass('open')
   })
 
-  $('.book-list-close').click(function() {
+  $('.book-list-close').click(function () {
     $(this).removeClass('open')
     $('.read').removeClass('open')
   })
 });
 
-$(window).load(function() {
+$(window).load(function () {
   $('body').addClass('active')
 })
 
 window.addEventListener('DOMContentLoaded', () => {
-
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav a[href="#${id}"]`).classList.add('active');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document.querySelector(`nav a[href="#${id}"]`).classList.add('active');
         document.title = `Noah Shrader - ${id}`;
-			} else {
-				document.querySelector(`nav a[href="#${id}"]`).classList.remove('active');
-			}
-		});
-	});
-
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('div[id]').forEach((section) => {
-		observer.observe(section);
-	});
-
+      } else {
+        document.querySelector(`nav a[href="#${id}"]`).classList.remove('active');
+      }
+    });
+  });
+  document.querySelectorAll('div[id]').forEach((section) => {
+    observer.observe(section);
+  });
 });
+
+$(window).on( 'scroll', function(){
+  if ($(window).scrollTop() < 10) {
+    $(document).prop('title', 'Noah Shrader');
+  }
+})
