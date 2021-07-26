fc40648dc981b91076d1ee54cc50c7584ea36e80
Noah Shrader <noahshrader@gmail.com>
2021-07-26 19:50:14 -0400

    Begin new site structure

--- js/site.js
+++ js/site.js
@@ -16,26 +16,3 @@ $(window).load(function () {
     $('body').addClass('close')
   })
 })
-
-window.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const id = entry.target.getAttribute('id');
-      if (entry.intersectionRatio > 0) {
-        document.querySelector(`nav a[href="#${id}"]`).classList.add('active');
-        document.title = `Noah Shrader - ${id}`;
-      } else {
-        document.querySelector(`nav a[href="#${id}"]`).classList.remove('active');
-      }
-    });
-  });
-  document.querySelectorAll('div[id]').forEach((section) => {
-    observer.observe(section);
-  });
-});
-
-$(window).on( 'scroll', function(){
-  if ($(window).scrollTop() < 10) {
-    $(document).prop('title', 'Noah Shrader');
-  }
-})
